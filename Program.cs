54a6b2b5bc829fb32d00309cb1f9f27a1928971e
Hellifax <hellifax91@gmail.com>
2021-04-29 22:47:55 +0300

    add Project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace homeWork_1
+{
+    class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Hello {name}, today is {DateTime.Now}");
+        }
+    }
+}
